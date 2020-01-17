a54e63e3fc62442a2037ea444642b178e6ba4e1f
akaKAIN <mkain@mail.ru>
2020-01-18 01:27:24 +1000

    Реализовано чтение ссылок из заданного файла. Скачка и сохранение.

--- src/main.go
+++ src/main.go
@@ -1,34 +1,98 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"sync"
 )
 
-func main(){
-	url := os.Args[1]
-	format := "mp4"
-	if err := LoadAndSaveFile(url, format); err != nil{
-		log.Printf("Ошибка загрузчика по ссылке %s. Error: %s", url, err)
+const format = "mp4"
+
+func main() {
+	var (
+		wg       sync.WaitGroup
+		urls     []string
+		fileName string
+		counter int
+	)
+	switch len(os.Args) {
+	case 1:
+		fileName = "urls.txt"
+	case 2:
+		fileName = os.Args[1]
+	default:
+		log.Println("Неверно указаны параметры команды.")
+		os.Exit(1)
 	}
+
+	urls, err := GetUrls(fileName)
+	fmt.Printf("Получено ссылок из файла: %d\n", len(urls))
+	if err != nil {
+		log.Fatalf("Ошибка чтения файла: %s\n", err)
+	}
+	for _, url := range urls {
+		if url != ""{
+			counter++
+			wg.Add(1)
+			go LoadAndSaveFile(url, counter, format, &wg)
+		}
+
+	}
+	wg.Wait()
+	log.Println("Все ссылки прошли загрузку.")
 }
 
+func GetUrls(fileName string) ([]string, error) {
+	var urls = make([]string, 8)
 
-func LoadAndSaveFile(url string, format string) error{
+	fileBody, err := os.Open(fileName)
+	if err != nil {
+		err = fmt.Errorf("Ошибка открытия файла (%s)\n", err)
+		return nil, err
+	}
+	defer fileBody.Close()
+
+	r := bufio.NewReader(fileBody)
+	for true {
+		byteRow, _, _ := r.ReadLine()
+		if len(byteRow) == 0 {
+			break
+		}
+		urls = append(urls, string(byteRow))
+
+	}
+	return urls, nil
+}
+
+func LoadAndSaveFile(url string, index int, format string, wg *sync.WaitGroup) {
+	var strIndex, fileName string
+	defer wg.Done()
+
+	strIndex = strconv.Itoa(index)
+	fileName = "video_" + strIndex + "." + format
+	if url == ""{
+		err := fmt.Errorf("Получен пустой адресс ссылки.")
+		log.Println(err)
+	}
 	resp, err := http.Get(url)
 	if err != nil {
-		return err
+		err = fmt.Errorf("Ошибка получения ответа от сервера (%v)\n", err)
+		log.Println(err)
 	}
 	defer resp.Body.Close()
+	log.Printf("Скачивание по ссылке: %s\n", url)
 
-	newFile, err := os.Create("fileName." + format)
-	if err != nil{
-		return err
+	newFile, err := os.Create(fileName)
+	if err != nil {
+		log.Println(err)
 	}
 	defer newFile.Close()
 
 	_, err = io.Copy(newFile, resp.Body)
-	return err
+	log.Println("Файл успешно сохранен.")
 }
